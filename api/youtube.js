b0f8558230f97e8df0b7e56bae63e294b7365d24
bigjesi <daniel.jesi@gmail.com>
2026-05-05 21:39:48 -0300

    Add files via upload

---
+++ api/youtube.js
@@ -0,0 +1,28 @@
+﻿export default async function handler(req, res) {
+  try {
+    const channelId = "UCYPM1qFmrvpP2T_8GFOa0JA";
+
+
+    const response = await fetch(
+      `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`
+    );
+
+
+    const xml = await response.text();
+
+
+    const videoIdMatch = xml.match(/<yt:videoId>(.*?)<\/yt:videoId>/);
+    const videoId = videoIdMatch ? videoIdMatch[1] : "";
+
+
+    return res.status(200).json({
+      youtubeVideoId: videoId
+    });
+
+
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+}
