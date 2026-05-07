2c528d91eec758155240f3323d646c685c3a0fae
bigjesi <daniel.jesi@gmail.com>
2026-05-06 21:27:02 -0300

    Delete api/latest-content.js

--- api/latest-content.js
+++
@@ -1,8 +0,0 @@
-﻿export default async function handler(req, res) {
-return res.status(200).json({
-tiktokUrl: "https://www.tiktok.com/@bigjesi/video/EXEMPLO",
-instagramBigJesiUrl: "",
-instagramBbsLabUrl: "",
-youtubeVideoId: "caydiGxahzA"
-});
-}
