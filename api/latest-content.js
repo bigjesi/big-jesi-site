0d8eda628038a099bfad7d2a45f66b694b328616
bigjesi <daniel.jesi@gmail.com>
2026-05-06 21:15:06 -0300

    Add files via upload

---
+++ api/latest-content.js
@@ -0,0 +1,8 @@
+﻿export default async function handler(req, res) {
+return res.status(200).json({
+tiktokUrl: "https://www.tiktok.com/@bigjesi/video/EXEMPLO",
+instagramBigJesiUrl: "",
+instagramBbsLabUrl: "",
+youtubeVideoId: "caydiGxahzA"
+});
+}
