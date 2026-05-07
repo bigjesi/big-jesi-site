faffa1af6f030c1edc5feed4f30ac11ab260353e
bigjesi <daniel.jesi@gmail.com>
2026-05-06 21:53:20 -0300

    Update latest-content.js

--- api/latest-content.js
+++ api/latest-content.js
@@ -1,28 +1,19 @@
-﻿export default async function handler(req, res) {
-  try {
-    const channelHandle = "jesiennes";
+export default async function handler(req, res) {
 
+  return res.status(200).json({
 
-    const response = await fetch(`https://www.youtube.com/@${channelHandle}/shorts`);
-    const html = await response.text();
+    tiktokUrl:
+      "https://www.tiktok.com/@bigjesi/video/7628107238539889938",
 
+    instagramBigJesiUrl:
+      "https://www.instagram.com/p/DLgTzSXNn9b/",
 
-    const match = html.match(/"videoId":"([^"]+)"/);
-    const latestShortsId = match ? match[1] : "guJcyQG44pE";
+    instagramBbsLabUrl:
+      "",
 
+    youtubeVideoId:
+      "guJcyQG44pE"
 
-    return res.status(200).json({
-      tiktokUrl: "https://www.tiktok.com/@bigjesi/video/EXEMPLO",
-      instagramBigJesiUrl: "",
-      instagramBbsLabUrl: "",
-      youtubeVideoId: latestShortsId
-    });
-  } catch (error) {
-    return res.status(200).json({
-      tiktokUrl: "https://www.tiktok.com/@bigjesi/video/EXEMPLO",
-      instagramBigJesiUrl: "",
-      instagramBbsLabUrl: "",
-      youtubeVideoId: "guJcyQG44pE"
-    });
-  }
+  });
+
 }
